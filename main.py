60a16f84b9c337985ca7812ad9123425f91c9f6d
shahbaz <shbzshbr@gmail.com>
2018-07-20 19:30:36 +0500

    Main file updated

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
+import django.core.handlers.wsgi
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-
-import django.core.handlers.wsgi
 
 application = django.core.handlers.wsgi.WSGIHandler()
